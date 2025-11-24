fdb2152f8810c7f988e49046bfcdb7907f9bbd9d
davidalds <david57araujo@gmail.com>
2025-11-24 02:59:32 -0300

    change public footer

--- src/pages/public/components/ui/publicFooter.tsx
+++ src/pages/public/components/ui/publicFooter.tsx
@@ -1,7 +1,9 @@
+import { SiVite, SiTypescript, SiNestjs, SiReact } from 'react-icons/si'
 function PublicFooter() {
   return (
-    <footer className="p-2 text-center bg-secondary text-secondary-foreground mt-4">
-      Desenvolvido com Preguiça...
+    <footer className="flex justify-center items-center p-3 bg-secondary text-secondary-foreground mt-4 text-lg gap-2">
+      <span className="font-medium">Desenvolvido com:</span> <SiReact /> +{' '}
+      <SiVite /> + <SiNestjs /> + <SiTypescript />
     </footer>
   )
 }
